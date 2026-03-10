89d8d02d549942d425030bf603ab590ac2280443
Kushism536 <kushiseegakulamaheshbabbu@gmail.com>
2026-03-11 02:18:26 +0530

    Implement UC7: Encapsulated character patterns using static inner class and getters

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,19 +1,61 @@
 public class OOPSBannerApp {
 
+    // Inner Class
+    static class CharacterPatternMap {
+        char character;
+        String[] pattern;
+
+        CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+    }
+
     public static void main(String[] args) {
-        // Line 1
-        System.out.println(String.join("", "  *** ", "  *** ", " ***** ", " **** "));
-        // Line 2
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** ** "));
-        // Line 3
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ** ", " ** "));
-        // Line 4
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ***** ", "  *** "));
-        // Line 5
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", "    ** "));
-        // Line 6
-        System.out.println(String.join("", " ** ** ", " ** ** ", " ** ", " ** ** "));
-        // Line 7
-        System.out.println(String.join("", "  *** ", "  *** ", " ** ", "  **** "));
+
+        String[] oPattern = {
+            "   ***   ",
+            " **   ** ",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            "**     **",
+            " **   ** ",
+            "   ***   "
+        };
+
+        String[] pPattern = {
+            "******   ",
+            "**    ** ",
+            "**     **",
+            "**    ** ",
+            "******   ",
+            "**       ",
+            "**       ",
+            "**       ",
+            "**       "
+        };
+
+        String[] sPattern = {
+            "   ***** ",
+            " **      ",
+            "**       ",
+            " **      ",
+            "   ***   ",
+            "      ** ",
+            "       **",
+            "      ** ",
+            " *****   "
+        };
+
+        // Store patterns in objects
+        CharacterPatternMap o = new CharacterPatternMap('O', oPattern);
+        CharacterPatternMap p = new CharacterPatternMap('P', pPattern);
+        CharacterPatternMap s = new CharacterPatternMap('S', sPattern);
+
+        for (int i = 0; i < o.pattern.length; i++) {
+            System.out.println(o.pattern[i] + " " + o.pattern[i] + " " + p.pattern[i] + " " + s.pattern[i]);
+        }
     }
 }
