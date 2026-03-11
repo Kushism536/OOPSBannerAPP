723ffb2bf57f621087601a5abdd2cc9523dc2ee6
Kushism536 <kushiseegakulamaheshbabbu@gmail.com>
2026-03-11 13:00:09 +0530

    UC4: Refactor banner rendering to use String array and enhanced for loop

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,72 +1,18 @@
-import java.util.HashMap;
-
 public class OOPSBannerApp {
-
-    public static HashMap<Character, String[]> createCharacterMap() {
-        HashMap<Character, String[]> charMap = new HashMap<>();
-
-        charMap.put('O', new String[]{
-            "   ***   " ,
-            " **   ** " ,
-            " **   ** " ,
-            " **   ** " ,
-            " **   ** " ,
-            " **   ** " ,
-            "   ***   "
-        });
-
-        charMap.put('P', new String[]{
-            " ******   " ,
-            " **    ** " ,
-            " **    ** " ,
-            " ******   " ,
-            " **       " ,
-            " **       " ,
-            " **       " 
-        });
-
-        charMap.put('S', new String[]{
-            "   *****     " ,
-            "  **    **   " ,
-            " **          " ,
-            "   *****     " ,
-            "         **  " ,
-            " **     **   " ,
-            "   *****     "
-        });
-
-        charMap.put(' ', new String[]{
-            "    ",
-            "    ",
-            "    ",
-            "    ",
-            "    ",
-            "    ",
-            "    "
-        });
-
-        return charMap;
+     public static void main(String[] args) {
+        String[] lines = new String [10];
+    lines[0] =String.join("  ","   ***   ","   ***   ","******   ","   ***** ");
+    lines[1] =String.join("  "," **   ** "," **   ** ","**    ** "," **      ");
+    lines[2] =String.join("  ","**     **","**     **","**     **","**       ");
+    lines[3] =String.join("  ","**     **","**     **","**    ** "," **      ");
+    lines[4] =String.join("  ","**     **","**     **","******   ","   ***   ");
+    lines[5] =String.join("  ","**     **","**     **","**       ","      ** ");
+    lines[6] =String.join("  ","**     **","**     **","**       ","       **");
+    lines[7] =String.join("  "," **   ** "," **   ** ","**       ","      ** ");
+    lines[8] =String.join("  ","   ***   ","   ***   ","**       "," *****   ");
+    lines[9] =String.join("                                                  ");
+    for (String line : lines) {
+        System.out.println(line);
     }
-
-    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
-        int patternHeight = charMap.get('O').length;
-
-        for (int line = 0; line < patternHeight; line++) {
-            StringBuilder sb = new StringBuilder();
-            
-            for (char ch : message.toCharArray()) {
-                char upperCh = Character.toUpperCase(ch);
-                String[] pattern = charMap.getOrDefault(upperCh, charMap.get(' '));
-                sb.append(pattern[line]).append(" ");
-            }
-            
-            System.out.println(sb.toString());
-        }
-    }
-
-    public static void main(String[] args) {
-        HashMap<Character, String[]> charMap = createCharacterMap();
-        String message = "OOPS";
-        displayBanner(message, charMap);
-    }
+     }
 }
